fce5926aa16fa4f2fc72dfe7556e83bd7da96f7b
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2022-10-03 11:26:26 +0300

    Create Counting Valleys.py

---
+++ week-2/Counting Valleys.py
@@ -0,0 +1,12 @@
+def countingValleys(steps, path):
+   # Write your code here
+    valleys = 0
+    cur_level = 0
+    for steps in path:
+        if(steps == 'U'):
+            cur_level += 1
+            if(cur_level == 0):
+                valleys += 1
+        elif(steps == 'D'):
+            cur_level -= 1
+    return(valleys)
