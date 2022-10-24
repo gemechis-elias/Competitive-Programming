923afd98078c3f3817a5b21db34596421d95cf64
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2022-10-24 09:37:32 +0300

    added comment

--- week-1/Grading Students.py
+++ week-1/Grading Students.py
@@ -1,8 +1,9 @@
 def gradingStudents(grades):
-    for item in range(len(grades)):
-        if grades[item]>=38:
-            num1=grades[item]//5
-            num2=(num1+1)*5-grades[item]
-            if num2<3:
-                grades[item]=(num1+1)*5
+    # Write your code here
+    for i in range(len(grades)):
+        if grades[i]>=38:   #checking if grade is above 38
+            x=grades[i]//5   #Floor devision 
+            difference=(x+1)*5-grades[i]    #calculating difference
+            if difference<3:
+                grades[i]=(x+1)*5
     return grades
