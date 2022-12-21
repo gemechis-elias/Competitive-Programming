c2c0cfd5057cf72e54e32a4a2b4d886bee5a2b13
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2022-12-21 17:46:14 +0300

    Create python-division.py

---
+++ Python Track/python-division.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+def int_division(a, b):
+    return a//b
+def float_divsion(a, b):
+    return a/b
+
+print(int_division(a,b))
+print(float_divsion(a,b))
+    
