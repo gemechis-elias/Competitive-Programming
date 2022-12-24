6fe569a93768c61aaec528e19d8c5d9de1be8dbf
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2022-12-24 12:06:33 +0300

    Time: 32 ms (89.43%), Space: 13.8 MB (96.72%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,13 +1,11 @@
 class Solution:
     def interpret(self, command: str) -> str:
         res = ''
-        i = 0
-        while len(command) > i:
+        
+        for i in range(0, len(command)):
             if command[i] == "(":
                 if len(command) > i+1 and command[i+1] == ")":
-                    res+="o"
-                    i+=1
+                    res +="o"
             elif command[i] != ")":
-                res+= command[i]
-            i+=1
+                res += command[i]
         return res
