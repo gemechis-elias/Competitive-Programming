37e50ec00153f22d00642a17aa11755721b82358
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2022-12-30 22:38:23 +0300

    Update multiply-strings.py

--- Python Track/multiply-strings.py
+++ Python Track/multiply-strings.py
@@ -1,4 +1,12 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        ans=str(int(num1)*int(num2))
-        return ans
+        num_1 = 0
+        num_2 = 0
+        for i in num1: 
+            num_1 = num_1 * 10 + (ord(i) - 48) 
+        for j in num2: 
+            num_2 = num_2 * 10 + (ord(j) - 48) 
+
+        mul = num_1*num_2
+        
+        return str(mul)
