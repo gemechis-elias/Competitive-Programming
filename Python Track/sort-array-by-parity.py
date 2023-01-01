18b9c1215fd11a35a61ffade1ffd089ead0ec94e
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-01-01 13:36:03 +0300

    Create sort-array-by-parity.py

---
+++ Python Track/sort-array-by-parity.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        l = 0
+        for i in range(len(nums)):
+            if (nums[i] % 2)==0:
+                nums[l], nums[i] = nums[i], nums[l]
+                l +=1
+        return nums
