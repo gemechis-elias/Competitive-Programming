fb15b691c4f8150cc21bab2f7e6a9335a26a840a
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-01-02 13:56:39 +0300

    Create Set_difference.py

---
+++ Python Track/Set_difference.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+E = int(input())
+eng= set(input().split())
+F = int(input())
+french = set(input().split())
+print(len(eng.difference(french)))
