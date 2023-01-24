5b563abe1f022c36a476c2b4e1a200db470200bd
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-01-24 19:44:33 +0300

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        start = 0
+        end = len(numbers)-1
+        while start != end:
+            if (numbers[start] + numbers[end]) == target:
+                idx = [start+1, end+1]
+                return idx
+            elif (numbers[start] + numbers[end]) < target:
+                start +=1
+            elif (numbers[start] + numbers[end]) > target:
+                end -=1
