755040b10baba25dbf854adf6c7eae1daac73ff8
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-01-27 19:30:32 +0300

    Create maxOperations.py

---
+++ maxOperations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        right = len(nums)-1
+        count = 0
+        while left<right:
+            if (nums[left] + nums[right])<k:
+                left+=1
+            elif (nums[left]+nums[right])>k:
+                right-=1
+            else:
+                count+=1
+
+                right-=1
+                left+=1
+        return count
