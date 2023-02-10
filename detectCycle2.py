21eba4da2477d620537f22cc0b97bd0459ef72a3
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-02-10 17:57:48 +0300

    Create detectCycle2.py

---
+++ detectCycle2.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+        flag = True
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        if not fast or not fast.next:
+            return 
+        else:
+            cur = head
+            while slow != cur:
+                slow = slow.next
+                cur = cur.next
+        return cur
