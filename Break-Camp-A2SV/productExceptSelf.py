4d1ff773f1fff44d7a07b45b2f49fd5adbaaa24f
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-02-24 17:03:46 +0300

    Create productExceptSelf.py

---
+++ Break-Camp-A2SV/productExceptSelf.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        output = [1] * n
+        prefixmul = 1
+        postfixmul = 1
+        index = n-1
+
+        for i in range(1,n):
+            prefixmul *= nums[i-1]
+            output[i] = prefixmul
+        print(output)    
+        for i in range(n-2,-1,-1):
+            postfixmul*=nums[i+1]
+            output[i]*=postfixmul
+        print(output)  
+        return output
