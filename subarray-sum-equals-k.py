721109e8a86533e78eb234380338e79cd92d81f6
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-03-01 17:14:12 +0300

    added subarray-sum-equals-k.python

---
+++ subarray-sum-equals-k.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_count={0:1}
+        count_subarray = 0
+        tempsum = 0
+
+        for i in nums:
+            tempsum += i
+            temp = tempsum-k
+            if temp in prefix_count: 
+                count_subarray += prefix_count[temp]
+                
+            if tempsum in prefix_count: 
+                prefix_count[tempsum] += 1
+                
+            else: 
+                prefix_count[tempsum]=1
+        return count_subarray
