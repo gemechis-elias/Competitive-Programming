f02a8ae01e5a819637f9bc79959543cb11ce3cfd
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-03-06 15:24:39 +0300

    added grid-game.python

---
+++ grid-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        n = len(grid[0])
+        ans = math.inf
+        topSum = sum(grid[0])
+        bottomSum = 0
+
+        for i in range(n):
+            topSum -= grid[0][i]
+            ans = min(ans, max(topSum, bottomSum))
+            bottomSum += grid[1][i]
+            print(ans, topSum, bottomSum)
+
+        return ans
