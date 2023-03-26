810ce9e937f7d316e9a0c5458ab45a520a8fd301
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-03-26 20:06:08 +0300

    added replace-the-substring-for-balanced-string.python

---
+++ replace-the-substring-for-balanced-string.py
@@ -0,0 +1,11 @@
+def balancedString(self, s):
+        count = collections.Counter(s)
+        res = n = len(s)
+        i = 0
+        for j, c in enumerate(s):
+            count[c] -= 1
+            while i < n and all(n / 4 >= count[c] for c in 'QWER'):
+                res = min(res, j - i + 1)
+                count[s[i]] += 1
+                i += 1
+        return res
