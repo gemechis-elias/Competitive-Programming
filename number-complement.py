c2d2e5d846c2433509cc6346b3a470ce1825985e
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-03-30 18:23:37 +0300

    added number-complement.python

--- number-complement.py
+++ number-complement.py
@@ -1,18 +1,7 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        str_bit = bin(num)
-        bit_ = list(str_bit)
-        
-        for i in range(2, len(bit_)):
-            if bit_[i] == "1":
-                bit_[i] = "0"
-            else:
-                bit_[i] = "1"
-        
-        ans = ""
-        for i in bit_[2:]:
-            ans += i
-        
+        num_bits = num.bit_length()
+        #print(num_bits)
+        complement = ~num & ((1 << num_bits) - 1)
 
-
-        return int(ans, 2)
+        return complement
