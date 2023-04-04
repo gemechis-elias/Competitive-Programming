d5f03e5c035c1cf67928c9dadaef87da37f18dff
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-04-05 00:04:44 +0300

    added find-greatest-common-divisor-of-array.python

---
+++ find-greatest-common-divisor-of-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        def gcd(a, b):
+            if b == 0:
+                return a
+            return gcd(b, a % b)
+
+        return gcd(min(nums), max(nums))
