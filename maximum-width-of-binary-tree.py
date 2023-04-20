bec96b9f3eda3fe18b9eb98252b0ada1941f87dc
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-04-20 22:01:14 +0300

    added maximum-width-of-binary-tree.python

--- maximum-width-of-binary-tree.py
+++ maximum-width-of-binary-tree.py
@@ -13,9 +13,10 @@ class Solution:
                 return root
             
             dic[level].append(column)
+           
             check(root.left, level + 1, column * 2)
             check(root.right, level + 1, (column * 2) + 1)
                 
         check(root, 0 , 0)
-        
+       
         return max([max(dic[level]) - min(dic[level]) + 1 for level in dic])
