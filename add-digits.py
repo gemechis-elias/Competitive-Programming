706a82c1fb1f1f66b8e78b23722b437111fb7670
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-04-26 06:56:39 +0300

    added add-digits.python

---
+++ add-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            sum = 0
+            while num:
+                sum += num%10
+                num = num//10
+                
+            num = sum
+
+        return num
