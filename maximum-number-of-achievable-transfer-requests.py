e8440bc44feef362eb96c78890ecaefa9b1000cd
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-04-30 14:20:05 +0300

    added maximum-number-of-achievable-transfer-requests.python

--- maximum-number-of-achievable-transfer-requests.py
+++ maximum-number-of-achievable-transfer-requests.py
@@ -1,24 +1,19 @@
 class Solution:
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
-        node_counts = [0] * n
         total_requests = len(requests)
         self.max_requests = 0
         
-        def backtrack(i, count):
-            if i == total_requests:
-                if all(c == 0 for c in node_counts):
-                    self.max_requests = max(self.max_requests, count)
-                return
+        for mask in range(1 << total_requests):
+            node_counts = [0] * n
+            count = 0
             
-            # Skip current request
-            backtrack(i+1, count)
+            for i in range(total_requests):
+                if mask & (1 << i):
+                    node_counts[requests[i][0]] -= 1
+                    node_counts[requests[i][1]] += 1
+                    count += 1
+                    
+            if all(c == 0 for c in node_counts):
+                self.max_requests = max(self.max_requests, count)
             
-            # Perform current request
-            node_counts[requests[i][0]] -= 1
-            node_counts[requests[i][1]] += 1
-            backtrack(i+1, count+1)
-            node_counts[requests[i][0]] += 1
-            node_counts[requests[i][1]] -= 1
-            
-        backtrack(0, 0)
         return self.max_requests
