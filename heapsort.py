42335b08a70626ed30390785960f78872f296d4b
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-05-03 12:21:39 +0300

    Create heapsort.py

---
+++ heapsort.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+class Solution:
+    # Heapify function to maintain heap property.
+    def heapify(self, arr, n, i):
+        largest = i
+        l = 2 * i + 1  # left = 2*i + 1
+        r = 2 * i + 2  # right = 2*i + 2
+        if l < n and arr[l] > arr[largest]:
+            largest = l
+        if r < n and arr[r] > arr[largest]:
+            largest = r
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            self.heapify(arr, n, largest)
+
+    # Function to build a Heap from array.
+    def buildHeap(self, arr, n):
+        # Index of last non-leaf node
+        startIdx = n // 2 - 1
+        # each node
+        for i in range(startIdx, -1, -1):
+            self.heapify(arr, n, i)
+
+    # Function to sort an array using Heap Sort.
+    def HeapSort(self, arr, n):
+        n = len(arr)
+        self.buildHeap(arr, n)
+        for i in range(n - 1, 0, -1):
+            arr[i], arr[0] = arr[0], arr[i]  # swap
+            self.heapify(arr, i, 0)
+
+    
