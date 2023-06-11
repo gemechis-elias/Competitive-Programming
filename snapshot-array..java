d2cab19bdb8ba6047f4f9f897d95a32b4d5b2032
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-06-11 13:43:54 +0300

    added snapshot-array.java

--- snapshot-array..java
+++ snapshot-array..java
@@ -1,23 +1,33 @@
-class SnapshotArray {
-    TreeMap<Integer, Integer>[] Tm;
-    int snap_id = 0;
-    public SnapshotArray(int length) {
-        Tm = new TreeMap[length];
-        for (int i = 0; i < length; i++) {
-            Tm[i] = new TreeMap<Integer, Integer>();
-            Tm[i].put(0, 0);
-        }
-    }
+class SnapshotArray:
 
-    public void set(int index, int val) {
-        Tm[index].put(snap_id, val);
-    }
+    def __init__(self, length: int):
+        self.cur_id = 0
+        self.curVals = [0] * length
+        self.snapIdArr = [[-1] for _ in range(length)]
+        self.arrVal = [[0] for _ in range(length)]
+        self.modified = set()
 
-    public int snap() {
-        return snap_id++;
-    }
+    def set(self, index: int, val: int) -> None:
+        if val == self.arrVal[index][-1]:
+            if index in self.modified: self.modified.remove(index)
+            return
+        self.curVals[index] = val
+        if index not in self.modified: self.modified.add(index)
 
-    public int get(int index, int snap_id) {
-        return Tm[index].floorEntry(snap_id).getValue();
-    }
-}
+    def snap(self) -> int:
+        for idx in self.modified:
+            self.snapIdArr[idx].append(self.cur_id)
+            self.arrVal[idx].append(self.curVals[idx])
+        self.modified.clear()
+        self.cur_id += 1
+        return self.cur_id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        arr = self.snapIdArr[index]
+        l, r = 0, len(arr)
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] <= snap_id:
+                l = m + 1
+            else: r = m
+        return self.arrVal[index][l-1]
