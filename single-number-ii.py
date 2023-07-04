4a5bede021c2f0974d8a2dacfe68104a22bb6016
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-07-04 20:17:49 +0300

    added single-number-ii.python

--- single-number-ii.py
+++ single-number-ii.py
@@ -1,3 +1,10 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        return (3 * sum(set(nums)) - sum(nums)) // 2
+    def singleNumber(self, nums):
+        single = 0
+        for i in range(32):
+            count = 0
+            for num in nums:
+                if num & (1<<i) == (1<<i): count += 1
+            single |= (count%3) << i
+            
+        return single if single < (1<<31) else single - (1<<32)
