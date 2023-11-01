193aa452731e8376d8fb95e2c018e6535779ca0e
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-11-01 21:56:33 +0300

    Added solution for Maximum Population Year

---
+++ interview-prep/maximum-population-year.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        return self.solve(logs)
+
+    def solve(self, logs):
+        years = [0] * 101    
+        for log in logs:
+            birth_idx = log[0] - 1950
+            death_idx = log[1] - 1950
+
+            for i in range(birth_idx, death_idx):
+                years[i] += 1
+        
+        print(years)
+
+        max_pop = 0
+        max_idx = 0
+
+        for idx,year in enumerate(years):
+            if year > max_pop:
+                max_pop = year
+                max_idx = idx
+        
+
+        return max_idx + 1950
+
+
